93a2e7bd4cbe9e1fcdeaeb1e0faf3474997aaf8f
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-09-11 15:16:47 -0400

    installing tailwind, apparently

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Test from "@/components/Test";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center bg-red-950 h-screen">
+    <main className="flex flex-col items-center justify-center bg-green-950 h-screen">
       {/* <Test /> */}
       <Card />
+      <h3 className="text-red-500 uppercase font-bold text-5xl">Hello!</h3>
     </main>
   );
 }
